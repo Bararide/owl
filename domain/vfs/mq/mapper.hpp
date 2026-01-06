cbaa50db5a10c868564cc9a5c3dce3ab3f564773
Bararide <445467246r@gmail.com>
2026-01-07 01:49:07 +0300

    refactoring mqobserver

---
+++ domain/vfs/mq/mapper.hpp
@@ -0,0 +1,53 @@
+#ifndef OWL_MQ_MAPPER
+#define OWL_MQ_MAPPER
+
+#include "vfs/core/loop/simple_separate_thread.hpp"
+#include "vfs/mq/controllers/controllers.hpp"
+#include "vfs/mq/zeromq_loop.hpp"
+
+namespace owl {
+
+static std::pair<Verb, std::string> mqmap(const std::string &verb_str) {
+  static const std::unordered_map<std::string_view,
+                                  std::pair<Verb, std::string_view>>
+      routes{
+          {"container_create",                {Verb::Post, "container/create"}},
+          {"get_container_files",             {Verb::Get, "container/files"}},
+          {"get_container_files_and_rebuild", {Verb::Get, "container/files"}},
+          {"container_delete",                {Verb::Delete, "container/delete"}},
+          {"file_create",                     {Verb::Post, "file/create"}},
+          {"create_file",                     {Verb::Post, "file/create"}},
+          {"file_delete",                     {Verb::Delete, "file/delete"}},
+          {"delete_file",                     {Verb::Delete, "file/delete"}},
+          {"container_stop",                  {Verb::Post, "container/stop"}},
+          {"semantic_search_in_container",    {Verb::Post, "search/semantic"}},
+          {"semantic_search",                 {Verb::Post, "search/semantic"}}};
+
+  auto it = routes.find(verb_str);
+  return it != routes.end()
+             ? std::pair{it->second.first, std::string{it->second.second}}
+             : throw std::runtime_error("Unknown command: " + verb_str);
+}
+
+template <typename Derived> class MQMapper {
+public:
+  MQMapper(State &state, std::shared_ptr<ZeroMQLoop> loop)
+      : state_(state), dispatcher_(state), loop_(std::move(loop)) {}
+
+  template <typename... Args> void processMessage(Args &&...args) {
+    static_cast<Derived &>(*this)(std::forward<Args>(args)...);
+  }
+
+  std::shared_ptr<ZeroMQLoop> getLoop() const { return loop_; }
+
+private:
+  State &state_;
+  MQDispatcher dispatcher_;
+  std::shared_ptr<ZeroMQLoop> loop_;
+
+  friend Derived;
+};
+
+} // namespace owl
+
+#endif // OWL_MQ_MAPPER
--- domain/vfs/mq/observer.hpp
+++ domain/vfs/mq/observer.hpp
@@ -1,98 +1,45 @@
 #ifndef OWL_MQ_OBSERVER
 #define OWL_MQ_OBSERVER
 
-#include "vfs/mq/controllers/controllers.hpp"
-#include "vfs/core/loop/simple_separate_thread.hpp"
-#include "vfs/mq/zeromq_loop.hpp"
+#include "mapper.hpp"
 
 namespace owl {
 
 class MQObserver {
+  class Handler final : public MQMapper<Handler> {
+  public:
+    using Base = MQMapper<Handler>;
+    using Base::Base;
+
+    void operator()(const std::string &verb_str, const std::string &path_str,
+                    const nlohmann::json &msg) {
+      try {
+        auto [verb, path] = mqmap(verb_str);
+        Request req{verb, path, msg};
+
+        dispatcher_.dispatch(req);
+
+      } catch (const std::exception &e) {
+        std::string id = msg.value("request_id", "");
+        loop_->sendResponse(id, false, {{"error", e.what()}});
+        spdlog::error("MQ error: {}", e.what());
+      }
+    }
+  };
+
 public:
   explicit MQObserver(State &state)
-      : state_(state), dispatcher_(state),
-        zeromq_loop_(std::make_shared<ZeroMQLoop>(
-            [this](auto verb_str, auto path_str, auto msg) {
-              processMessage(verb_str, path_str, msg);
-            })),
-        runner_(zeromq_loop_) {}
+      : handler_(state,
+                 std::make_shared<ZeroMQLoop>([this](auto v, auto p, auto m) {
+                   handler_.processMessage(v, p, m);
+                 })),
+        runner_(handler_.getLoop()) {}
 
   void start() { runner_.start("mq_listener"); }
-
   void stop() { runner_.stop(); }
 
-  void sendResponse(const std::string &request_id, bool success,
-                    const nlohmann::json &data) {
-    if (zeromq_loop_) {
-      zeromq_loop_->sendResponse(request_id, success, data);
-    }
-  }
-
 private:
-  void processMessage(const std::string &verb_str, const std::string &path_str,
-                      const nlohmann::json &msg) {
-    try {
-      std::string request_id = msg.value("request_id", "");
-
-      auto [verb, path] = mapMQToRoute(verb_str, path_str, msg);
-
-      Request req{verb, path, msg};
-
-      spdlog::info("Processing MQ message: {} {} -> {} {}", verb_str, path_str,
-                   verbToString(verb), path);
-
-      dispatcher_.dispatch(req);
-
-    } catch (const std::exception &e) {
-      std::string request_id = msg.value("request_id", "");
-      sendResponse(request_id, false, {{"error", e.what()}});
-      spdlog::error("Error processing MQ message: {}", e.what());
-    }
-  }
-
-  std::pair<Verb, std::string> mapMQToRoute(const std::string &verb_str,
-                                            const std::string &path_str,
-                                            const nlohmann::json &msg) {
-    if (verb_str == "container_create") {
-      return {Verb::Post, "container/create"};
-    } else if (verb_str == "get_container_files" ||
-               verb_str == "get_container_files_and_rebuild") {
-      return {Verb::Get, "container/files"};
-    } else if (verb_str == "container_delete") {
-      return {Verb::Delete, "container/delete"};
-    } else if (verb_str == "file_create" || verb_str == "create_file") {
-      return {Verb::Post, "file/create"};
-    } else if (verb_str == "file_delete" || verb_str == "delete_file") {
-      return {Verb::Delete, "file/delete"};
-    } else if (verb_str == "container_stop") {
-      return {Verb::Post, "container/stop"};
-    } else if (verb_str == "semantic_search_in_container" ||
-               verb_str == "semantic_search") {
-      return {Verb::Post, "search/semantic"};
-    }
-
-    throw std::runtime_error("Unknown MQ command: " + verb_str);
-  }
-
-  std::string verbToString(Verb verb) {
-    switch (verb) {
-    case Verb::Get:
-      return "GET";
-    case Verb::Post:
-      return "POST";
-    case Verb::Put:
-      return "PUT";
-    case Verb::Delete:
-      return "DELETE";
-    default:
-      return "UNKNOWN";
-    }
-  }
-
-private:
-  State &state_;
-  MQDispatcher dispatcher_;
-  std::shared_ptr<ZeroMQLoop> zeromq_loop_;
+  Handler handler_;
   SimpleSeparateThreadLoopRunner<ZeroMQLoop> runner_;
 };
 
