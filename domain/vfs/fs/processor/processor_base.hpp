7550753c8dd194dfc67200c347bd4be800dcbf6a
Bararide <445467246r@gmail.com>
2026-01-09 01:35:57 +0300

    added dotenv lib

---
+++ domain/vfs/fs/processor/processor_base.hpp
@@ -0,0 +1,20 @@
+#ifndef OWL_FS_PROCESSOR_PROCESSOR_BASE
+#define OWL_FS_PROCESSOR_PROCESSOR_BASE
+
+namespace owl {
+
+class FSProcessor {
+public:
+    explicit FSProcessor() {
+        parseBaseDir();
+    }
+
+private:
+    void ParseBaseDir() {
+        
+    }
+};
+
+}
+
+#endif // OWL_FS_PROCESSOR_PROCESSOR_BASE
